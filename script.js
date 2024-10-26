790943ef43cde94a6a2fddc425e70e17d8343998
aisanaul <boyelarthur@gmail.com>
2024-10-26 22:14:50 +0530

    base layout done

---
+++ script.js
@@ -0,0 +1,9 @@
+function connect() {
+    opt1 = document.getElementById('option1');
+    opt2 = document.getElementById('option2');
+    opt3 = document.getElementById('option3');
+    opt4 = document.getElementById('option4');
+    opt5 = document.getElementById('option5');
+
+    
+}
